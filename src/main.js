7f61abc18d0e8b1af07d32a4eb9a275874bab404
derker <893309018@qq.com>
2016-08-26 15:28:11 +0800

    添加图标

--- src/main.js
+++ src/main.js
@@ -2,47 +2,50 @@ const electron = require('electron');
 const path = require('path');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-let win;
 
-function createWindow() {
+function openWindow() {
 
-	let pluginName
-	switch(process.platform) {
-		case 'win32':
-			pluginName = 'pepflashplayer.dll'
-			break
-		case 'darwin':
-			pluginName = 'PepperFlashPlayer.plugin'
-			break
-		case 'linux':
-			pluginName = 'libpepflashplayer.so'
-			break
-	}
-	app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName))
+    let pluginName;
+    switch (process.platform) {
+        case 'win32':
+            pluginName = 'pepflashplayer.dll';
+            break;
+        case 'darwin':
+            pluginName = 'PepperFlashPlayer.plugin';
+            break;
+        case 'linux':
+            pluginName = 'libpepflashplayer.so';
+            break;
+    }
+    app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName));
 
-	// Create the browser window.
-	win = new BrowserWindow({
-		width: 1200,
-		height: 800,
-		frame: false,
-		defaultFontSize: 16,
-		minWidth: 1200,
-		minHeight: 800,
-		defaultMonospaceFontSize: 16,
-		defaultEncoding: "utf-8",
-		webPreferences: {
-			plugins: true
-		}
-	});
+    // Create the browser window.
+    var win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        frame: false,
+        defaultFontSize: 16,
+        minWidth: 1200,
+        minHeight: 800,
+        icon: path.join(__dirname, 'img/icon.png'),
+        defaultMonospaceFontSize: 16,
+        defaultEncoding: "utf-8",
+        webPreferences: {
+            plugins: true
+        }
+    });
 
-	// and load the index.html of the app.
-	win.loadURL(`file://${__dirname}/index.html`);
+    // and load the index.html of the app.
+    win.loadURL(`file://${__dirname}/index.html`);
 
-	win.webContents.openDevTools();
+    //win.webContents.openDevTools();
 
-	win.on('closed', () => {
-		win = null;
-	});
+    win.on('closed', () => {
+        win = null;
+    });
 }
 
-app.on('ready', createWindow);
+app.on('ready', openWindow);
+app.on('window-all-closed', () => {
+    app.quit()
+});
